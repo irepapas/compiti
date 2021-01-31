e5eb30961f911ce5a0135edfa4af7f1ac07fe37e
Irene <Irene@Irene-HP.home>
2021-01-31 15:10:09 +0100

    start esercizio

--- src/esercizio3/App.java
+++ src/esercizio3/App.java
@@ -5,13 +5,26 @@
  */
 package esercizio3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Irene
  */
 public class App {
+
     public static void main(String[] args) {
-        
+            
+            
     }
-    
+  public static <T extends Number> List<T> minoriDi(T[] arr, T el) {
+        List<T> minori = new ArrayList<T>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].doubleValue() < el.doubleValue()) {
+                minori.add(arr[i]);
+            }
+        }
+        return minori;
+  }
 }
